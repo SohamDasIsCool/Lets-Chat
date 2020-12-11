e49ecef77c17bf25a5dcacfb21d735cf7b8c19a3
SohamDasIsCool <72563026+SohamDasIsCool@users.noreply.github.com>
2020-12-11 15:15:58 +0530

    Add files via upload

--- room.js
+++ room.js
@@ -8,12 +8,25 @@ var firebaseConfig = {
       appId: "1:498770365320:web:d2422786c538d6e91b883d"
     };
 
+    function addRoom() {
+      room_name=document.getElementById("room_name").value;
+      firebase.database().ref("/").child(room_name).update({purpose
+  :"Adding room name"});
+  localStorage.setItem("room_name",room_name);
+  window.location="kwitter_page.html";
+}
+
     firebase.initializeApp(firebaseConfig);
-    
 function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
        Room_names = childKey;
       //Start code
-
+      row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)'>#"+Room_names+"</div> <hr>";
+      document.getElementById("output").innerHTML+=row;
       //End code
       });});}
 getData();
+
+function redirectToRoomName(name) {
+      localStorage.setItem("room_name",name);
+      window.location="kwitter_page.html";
+}
